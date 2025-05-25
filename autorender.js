c325ac6be7f0c1270021f35e8de343ba48c2f33c
Cyrus Chan <cyruschan1212@gmail.com>
2025-05-25 23:03:43 +0800

    Publish index

---
+++ autorender.js
@@ -0,0 +1,8 @@
+"use strict";
+
+(function () {
+    const copyrightCtx = "Copyright &copy; 2025 rk0cc"
+    let copyrightElement = document.querySelector("footer.copyright");
+
+    copyrightElement.innerHTML = copyrightCtx;
+})();
