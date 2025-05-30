2cee25e0dcdb467af289d8063866c5f667a08ab5
Cyrus Chan <cyruschan1212@gmail.com>
2025-05-30 23:55:17 +0800

    Push simplified interface

--- autorender.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-(function () {
-    const copyrightCtx = "Copyright &copy; 2025 rk0cc"
-    let copyrightElement = document.querySelector("footer.copyright");
-
-    copyrightElement.innerHTML = copyrightCtx;
-})();
