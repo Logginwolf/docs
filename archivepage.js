a5a531321b7ef0d166f11e29ce497eb1252637f4
Cyrus Chan <cyruschan1212@gmail.com>
2025-07-07 23:10:57 +0800

    Work on recorded data

--- archivepage.js
+++ archivepage.js
@@ -1,15 +1,19 @@
 "use strict";
 
 (function () {
-    function linkSchemaSrc() {
+    function getAppliedEdition() {
         var appliedEdition = document.querySelector("html").getAttribute("data-logginwolf-archive-edition");
 
         if (typeof appliedEdition !== "string" || !/^[1-9][0-9]*$/.test(appliedEdition)) {
             throw new Error("Edition number unspecified");
         }
 
+        return appliedEdition;
+    }
+
+    function linkSchemaSrc() {
         var ghSrcLink = "https://github.com/Logginwolf/schemas/blob";
-        ghSrcLink += "/" + appliedEdition + "/preferences.json";
+        ghSrcLink += "/" + getAppliedEdition() + "/preferences.json";
 
         document.querySelector("a[data-jsonschema-gh-src]").setAttribute("href", ghSrcLink);
     }
